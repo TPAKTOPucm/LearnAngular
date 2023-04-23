11bd85b3f90872926e8b5d94cdaba2032e952963
Danila <danila24safonov@mail.ru>
2023-04-23 15:33:08 +0300

    add login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { CardGalleryComponent } from './card-gallery/card-gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CardComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    CardGalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+    {
+      path: '',
+      component: CardGalleryComponent
+    },
+    {
+      path: 'login',
+      component: LoginPageComponent
+    }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
